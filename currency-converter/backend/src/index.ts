92cc7a1e3745277f2a89de067584612ff8177ef3
davidchocholaty <david.chocholaty@seznam.cz>
2025-11-06 09:42:06 +0100

    Connect frontend to backend
    This commit adds API calls in the frontend to obtain the converted
    amount of money in the target currency.

--- currency-converter/backend/src/index.ts
+++ currency-converter/backend/src/index.ts
@@ -5,6 +5,18 @@ import { appRouter } from "./server/routers/_app.js";
 import { createContext } from "./server/context.js";
 
 const app = express();
+
+// Enable CORS for frontend
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 
 app.use(
